60172c5c363491136b8159d999bfa0886dea2d85
JoNoC0 <jnguyeningco@gmail.com>
2022-02-08 18:16:55 -0600

    create and implement user items

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@ap
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { setContext } from '@apollo/client/link/context';
 
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
@@ -16,8 +17,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
